697de679bbc668022a4743fdaeb238b9bb5d4a13
sushant <aryalsushant564@gmail.com>
2022-11-02 17:46:05 +0545

    Completed Initial setup of tailwind-Css

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../Layouts/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>We are in home page.</h1>
+    </Layout>
+  );
+};
+export default Home;
